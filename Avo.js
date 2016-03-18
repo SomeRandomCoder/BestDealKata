d22253b021b21d7d915eda224a329068ecaa55cf
Tyron <tyron.burkhardt.za@gmail.com>
2016-03-18 13:22:28 +0200

    finished with Avo

--- Avo.js
+++ Avo.js
@@ -27,82 +27,55 @@ console.log("----------------------")
 console.log(quantity)
 console.log(price)
 console.log("----------------------")
-
-
-for ( i = 0; i < quantity.length; i++)
+for (i = 0; i < quantity.length; i++)
 {
     data.push({
+      Deal: i + 1,
       quantity: Number(quantity[i]),
-      price: Number(price[i]),
+      price: Number(price[i])
     })
 }
 console.log(data);
 return data;
 };
-
   exports.perAvoPrice = function(data){ // amountIndex
     var singlePriceAvo =[];
-
     for (var x = 0; x < data.length; x++){
       var singleP = data[x].price / data[x].quantity;
       var dec = singleP.toFixed(2);
       var number = Number(dec);
       singlePriceAvo.push(number);
     }
-
     singlePriceAvo.join(",").split(",");
     console.log(singlePriceAvo);
     return singlePriceAvo;
-/*
-  var quantityArray = [amountIndex[0],amountIndex[2],amountIndex[4],amountIndex[6]];
-  var amountArray = [amountIndex[1],amountIndex[3],amountIndex[5],amountIndex[7]];
-
-  var dealOne = amountArray[0] / quantityArray[0];
-  var dl1 = dealOne.toFixed(2);
-  var dealTwo = amountArray[1] / quantityArray[1];
-  var dl2 = dealTwo.toFixed(2);
-  var dealThree = amountArray[2] / quantityArray[2];
-  var dl3 = dealThree.toFixed(2);
-  var dealFour = amountArray[3] / quantityArray[3];
-  var dl4 = dealFour.toFixed(2);
-
-  array =[];
-  array.push(dl1);
-  array.push(dl2);
-  array.push(dl3);
-  array.push(dl4);
-  console.log(array);
-  return array;*/
-
-};
-
-exports.cheapestDeal = function(singlePriceAvo) {
-  var min = Math.min.apply(Math, singlePriceAvo).toFixed(2);
-    for (y = 0; y < singlePriceAvo.length; y++) {
-      if (singlePriceAvo[y] == min) {
-        console.log(quantity[y] + " for R" + price[y]);
-        return quantity[y] + " for R" + price[y];
-      }
-    }
   };
 
-    // var min = Math.min.apply( Math, singlePriceAvo );
-    // min.toFixed(2);
-    // var min1 = Number(min);
-    // console.log( min1)S;
-    // return min1;
-
+exports.cheapestDeal = function(singlePriceAvo)
+{
+  for (var i=0; i<1; i++){
+      singlePriceAvo.sort();
+  }
+  console.log(singlePriceAvo[0]);
+  return singlePriceAvo[0];
+};
 
  exports.expensiveDeal = function(singlePriceAvo){
-   var max = Math.max.apply(Math, singlePriceAvo );
-   console.log(max.toFixed(2));
-   return max;
- };
+   var largest = 0;
 
- exports.averagePrice = function(array){
-   var sum = eval(array.join('+'));  //The eval() function evaluates JavaScript code represented as a string.
+for (i = 0; i < singlePriceAvo.length; i++) {
+    if (singlePriceAvo[i] > largest) {
+        largest = singlePriceAvo[i];
+    }
+}
+console.log(largest);
+return largest;
+};
+
+ exports.averagePrice = function(singlePriceAvo){
+   var sum = eval(singlePriceAvo.join('+'));  //The eval() function evaluates JavaScript code represented as a string.
    console.log("Total :" + sum.toFixed(2));
-   var avg = sum / array.length;
+   var avg = sum / singlePriceAvo.length;
    console.log("Average :" + avg.toFixed(2));
    return avg;
  };
--- Avo_test.js
+++ Avo_test.js
@@ -3,7 +3,7 @@ var Avo = require("./Avo.js");
 
 describe("converting", function(){
   it ("should split/divide the string and join it by a ',' ,and then remove the 'R' sign and 'for' word.", function(){
-    var result = Avo.converting("1 for R3, 2 for R7, 3 for R10, 5 for R14.50");
+        var result = Avo.converting("1 for R3, 2 for R7, 3 for R10, 5 for R14.50");
     assert.deepEqual(result, ['1', '3', '2', '7', '3', '10', '5', '14.50']);
 
   });
@@ -13,16 +13,18 @@ describe("indexMap", function(){
   it("should return the price and quantity of Avos in each deal", function(){
         var result = Avo.indexMap(['1', '3', '2', '7', '3', '10', '5', '14.50']);
         assert.deepEqual(result,
-    [ { quantity: 1, price: 3 },
-      { quantity: 2, price: 7 },
-      { quantity: 3, price: 10 },
-      { quantity: 5, price: 14.5 } ]);
+      [ { Deal: 1, quantity: 1, price: 3 },
+        { Deal: 2, quantity: 2, price: 7 },
+        { Deal: 3, quantity: 3, price: 10 },
+        { Deal: 4, quantity: 5, price: 14.5 } ]
+);
   });
 });
 
 
 describe("perAvoPrice", function(){
   it("should return the price of one avo in each deal", function(){
+
         var result = Avo.perAvoPrice(
           [ { quantity: 1, price: 3 },
             { quantity: 2, price: 7 },
@@ -35,11 +37,8 @@ describe("perAvoPrice", function(){
 
 describe("Cheapest Deal", function(){
   it("should return the cheapest deal of all Deals", function(){
-      var result = Avo.cheapestDeal([ { quantity: 1, price: 3 },
-        { quantity: 2, price: 7 },
-        { quantity: 3, price: 10 },
-        { quantity: 5, price: 14.5 } ]
-      );
+
+      var result = Avo.cheapestDeal(['3.00', '3.50', '3.33', '2.90']);
       assert.equal(result, "2.90");
   });
 });
@@ -53,7 +52,7 @@ describe("Expensive Deal", function(){
 
   describe ("averagePrice", function(){
       it("should return the average price throughout all deals",function(){
-        var result = Avo.averagePrice(array);
+        var result = Avo.averagePrice(['3.00', '3.50', '3.33', '2.90']);
         assert.equal(result, "3.1825");
       });
   });
