3ad1fd4aa03da6c2e15a007dbdd3fe55c6f11d9c
SomeRandomCoder <tyron.burkhardt.za@gmail.com>
2016-03-21 07:47:05 +0200

    Had to upload due to problems with the terminal

--- Fruition.js
+++ Fruition.js
@@ -1,84 +1,168 @@
- var jsonFruits = {
+exports.jsonFruits = function(){
+ var jsonFruits =
+ {
+  "woolingsworth": {
+    "apples": 4,
+    "bananas": 3,
+    "oranges": 12
+  },
 
-"apples":
-[
-  {shop:"woolingsworth", price: 4},
-  {shop: "chockers", price: 5},
-  {shop: "shopwrong", price: 2},
-  {shop: "kwakspar", price: 4}
-],
+  "chockers": {
+    "bananas": 2,
+    "apples": 5,
+    "oranges": 4
+  },
 
-"bananas":
-[
-  {shop: "woolingsworth", price: 3},
-  {shop: "chockers", price:2},
-  {shop: "pickle pay", price: 4},
-  {shop: "shopwrong", price:3}
+  "pickle pay": {
+    "bananas": 4,
+    "oranges": 7
+  },
 
-],
+  "shopwrong": {
+    "apples": 2,
+    "bananas": 3
+  },
 
-"oranges":
-[
-  {shop:"woolingsworth", price: 12},
-  {shop: "chockers", price: 4},
-  {shop: "pickle pay", price:7},
-  {shop: "kwakspar", price: 9}
-]
+  "kwakspar": {
+    "oranges": 9,
+    "apples": 4
+  }
+};
+return jsonFruits;
+};
 
-}
 
-  exports.cheapestSeller = function(jsonFruits){
-    jsonFruits.oranges.sort(function(a, b){
-    return a.price - b.price;
-  });
+// exports.sortFruits=function(jsonFruits){
+//   var apples =[];
+//   var oranges = [];
+//   var bananas = [];
+//
+//   for (var shop in jsonFruits){
+//     if(jsonFruits[shop].hasOwnProperty("oranges")) {
+//       oranges.push({
+//         'shop': shop,
+//         'price': jsonFruits[shop].oranges
+//       });
+//     }
+//   }
+//
+//   for (var shop in jsonFruits){
+//   if(jsonFruits[shop].hasOwnProperty("apples")) {
+//       apples.push({
+//         'shop': shop,
+//         'price': jsonFruits[shop].apples
+//       });
+//     }
+//   }
+//
+//     for(var shop in jsonFruits){
+//     if(jsonFruits[shop].hasOwnProperty("bananas")) {
+//       bananas.push({
+//         'shop': shop,
+//         'price': jsonFruits[shop].bananas
+//       });
+//     }
+//   }
+//
+//   // console.log(apples);
+//   // console.log(oranges);
+//   // console.log(bananas);
+//   return apples;
+//   return bananas;
+//   return oranges;
+// };
 
-  console.log(jsonFruits.oranges[0]);
-  return jsonFruits.oranges[0];
- };
 
- exports.ascendingApples = function(apples){
-   jsonFruits.apples.sort(function(a, b){
-   return a.price - b.price;
- });
- console.log(jsonFruits.apples);
- };
-
- exports.descendingApples = function(apples){
-   jsonFruits.apples.sort(function(a, b){
-     return b.price - a.price;
-   })
-   console.log(jsonFruits.apples);
-
- };
-
- exports.cheapestFruit = function(jsonFruits){
-   var fruits = [];
-var total = 0;
-var total1 =0;
-var total2 =0;
-
-for (i=0; i<jsonFruits.length; i++)
-{
- total += jsonFruits.apples[i].price;
- total1 += jsonFruits.bananas[i].price;
- total2 += jsonFruits.oranges[i].price;
-}
-
-      fruits.push({
-        Apples: total},
-        {Bananas: total1},
-        {Oranges: total2
+exports.cheapestSeller = function(jsonFruits) {
+  var orangeProvider = [];
+  for (var shop in jsonFruits) {
+    if (jsonFruits[shop].hasOwnProperty("oranges")) {
+      orangeProvider.push({
+        'shop': shop,
+        'price': jsonFruits[shop].oranges
       });
+    }
+  }
+  var oranges = [];
+  for (x = 0; x < orangeProvider.length; x++) {
+    oranges.push(orangeProvider[x].price);
+  }
+  var min = Math.min.apply(Math, oranges);
+  for (y = 0; y < orangeProvider.length; y++) {
+    if (oranges[y] == min) {
+      console.log(orangeProvider[y].shop + ": " + orangeProvider[y].price);
+      return orangeProvider[y].shop;
+    }
+  }
+};
 
 
-      for (var y = 0; y < fruits.length; y++){
-        fruits.sort();
-      }
-      console.log(fruits[0]);
-      return fruits[0];
+ exports.ascendingApples = function(jsonFruits){
+   var appleProvider = []
 
+   for (var shop in jsonFruits) {
+   if (jsonFruits[shop].hasOwnProperty("apples")) {
+     appleProvider.push({
+       'shop': shop,
+       'price': jsonFruits[shop].apples
+     });
+   }
+ }
+   var ascending = appleProvider.sort(function(a, b){
+    return a.price - b.price;
+ });
 
-   console.log(fruits);
-
-
+ console.log(ascending);
+ return ascending
  };
+
+ exports.descendingApples = function(jsonFruits){
+   var appleProvider = []
+
+   for (var shop in jsonFruits) {
+   if (jsonFruits[shop].hasOwnProperty("apples")) {
+     appleProvider.push({
+       'shop': shop,
+       'price': jsonFruits[shop].apples
+     });
+   }
+ }
+   var descending = appleProvider.sort(function(a, b){
+    return b.price - a.price;
+ });
+
+ console.log(descending);
+ return descending
+ };
+
+//  exports.cheapestFruit = function(jsonFruits){
+//    var fruits = [];
+// var total = 0;
+// var total1 =0;
+// var total2 =0;
+//
+// for (i=0; i<jsonFruits.length; i++)
+// {
+//  total += jsonFruits.apples[i].price;
+//  total1 += jsonFruits.bananas[i].price;
+//  total2 += jsonFruits.oranges[i].price;
+// }
+//
+//       fruits.push({
+//         Apples: total},
+//         {Bananas: total1},
+//         {Oranges: total2
+//       });
+//
+//
+//       for (var y = 0; y < fruits.length; y++){
+//         fruits.sort();
+//       }
+//       console.log(fruits[0]);
+//       return fruits[0];
+//
+//
+//    console.log(fruits);
+//
+//
+//  };
--- Fruition_test.js
+++ Fruition_test.js
@@ -1,125 +1,160 @@
 var assert = require('assert');
 var Fruit = require('./Fruition.js');
 
-describe("cheapest oranges", function(){
-    it("should find the cheapest seller of oranges", function(){
-      var result = Fruit.cheapestSeller(
-        {
+// describe("sortFruits", function(jsonFruits){
+//   it("should sort the data into fruit categories and display the shops that sell it", function(){
+//       var result = Fruit.sortFruits(
+//         {
+//         "woolingsworth": {
+//           "apples": 4,
+//           "bananas": 3,
+//           "oranges": 12
+//         },
+//
+//         "chockers": {
+//           "bananas": 2,
+//           "apples": 5,
+//           "oranges": 4
+//         },
+//
+//         "pickle pay": {
+//           "bananas": 4,
+//           "oranges": 7
+//         },
+//
+//         "shopwrong": {
+//           "apples": 2,
+//           "bananas": 3
+//         },
+//
+//         "kwakspar": {
+//           "oranges": 9,
+//           "apples": 4
+//         }
+//       });
+//       assert.equal(result,
+//
+//         [ { shop: 'woolingsworth', price: 4 },
+//         { shop: 'chockers', price: 5 },
+//         { shop: 'shopwrong', price: 2 },
+//         { shop: 'kwakspar', price: 4 } ],
+//
+//       [ { shop: 'woolingsworth', price: 12 },
+//         { shop: 'chockers', price: 4 },
+//         { shop: 'pickle pay', price: 7 },
+//         { shop: 'kwakspar', price: 9 } ],
+//
+//       [ { shop: 'woolingsworth', price: 3 },
+//         { shop: 'chockers', price: 2 },
+//         { shop: 'pickle pay', price: 4 },
+//         { shop: 'shopwrong', price: 3 } ]
+//     );
+//   });
+//
+// });
 
-       "apples":
-       [
-         {shop:"woolingsworth", price: 4},
-         {shop: "chockers", price: 5},
-         {shop: "shopwrong", price: 2},
-         {shop: "kwakspar", price: 4}
-       ],
-       "bananas":
-       [
-         {shop: "woolingsworth", price: 3},
-         {shop: "chockers", price:2},
-         {shop: "pickle pay", price: 4},
-         {shop: "shopwrong", price:3}
-
-       ],
-       "oranges":
-       [
-         {shop:"woolingsworth", price: 12},
-         {shop: "chockers", price: 4},
-         {shop: "pickle pay", price:7},
-         {shop: "kwakspar", price: 9}
-       ]
-
-       }
-      );
-      assert.deepEqual(result, {shop: "chockers", price: 4});
-    });
+describe("cheapestSeller",function(){
+  it("should return the provider of the cheapest oranges",function(){
+    var result = Fruit.cheapestSeller({
+      "woolingsworth": {
+        "apples": 4,
+        "bananas": 3,
+        "oranges": 12
+      },
+      "chockers": {
+        "bananas": 2,
+        "apples": 5,
+        "oranges": 4
+      },
+      "picklepay": {
+        "bananas": 4,
+        "oranges": 7
+      },
+      "shopwrong": {
+        "apples": 2,
+        "bananas": 3
+      },
+      "kwakspar": {
+        "oranges": 9,
+        "apples": 4
+}
+      });
+    assert.equal(result,"chockers");
+  });
 });
 
 describe("ascending apples", function(){
   it("should return the sellers of apples and the prices in ascending order", function(){
       var result = Fruit.ascendingApples(
         {
-       "apples":
-       [
-         {shop:"woolingsworth", price: 4},
-         {shotp: "chockers", price: 5},
-         {shop: "shopwrong", price: 2},
-         {shop: "kwakspar", price: 4}
-       ],
-       "bananas":
-       [
-         {shop: "woolingsworth", price: 3},
-         {shop: "chockers", price:2},
-         {shop: "pickle pay", price: 4},
-         {shop: "shopwrong", price:3}
-       ],
-       "oranges":
-       [
-         {shop:"woolingsworth", price: 12},
-         {shop: "chockers", price: 4},
-         {shop: "pickle pay", price:7},
-         {shop: "kwakspar", price: 9}
-       ]
-       }
-
-      );
-      assert.equal = (result,
+        "woolingsworth": {
+          "apples": 4,
+          "bananas": 3,
+          "oranges": 12
+        },
+        "chockers": {
+          "bananas": 2,
+          "apples": 5,
+          "oranges": 4
+        },
+        "picklepay": {
+          "bananas": 4,
+          "oranges": 7
+        },
+        "shopwrong": {
+          "apples": 2,
+          "bananas": 3
+        },
+        "kwakspar": {
+          "oranges": 9,
+          "apples": 4
+  }
+});
+      assert.equal(result,
         [
           {shop: "shopwrong", price: 2},
           {shop: "kwakspar", price: 4},
           {shop:"woolingsworth", price: 4},
-          {shop: "chockers", price: 5},
-        ]
+          {shop: "chockers", price: 5}]
       );
 
   });
 
 });
 
-
-
-
-
-
 describe("descending apples", function(){
   it("should return the sellers of apples and the prices in descending order by price.", function(){
-      var result = Fruit.descendingApples(
-        {
-       "apples":
-       [
-         {shop:"woolingsworth", price: 4},
-         {shotp: "chockers", price: 5},
-         {shop: "shopwrong", price: 2},
-         {shop: "kwakspar", price: 4}
-       ],
-       "bananas":
-       [
-         {shop: "woolingsworth", price: 3},
-         {shop: "chockers", price:2},
-         {shop: "pickle pay", price: 4},
-         {shop: "shopwrong", price:3}
-       ],
-       "oranges":
-       [
-         {shop:"woolingsworth", price: 12},
-         {shop: "chockers", price: 4},
-         {shop: "pickle pay", price:7},
-         {shop: "kwakspar", price: 9}
-       ]
-       }
-
-      );
-      assert.equal = (result,
-        [
-          {shop: "chockers", price: 5},
-          {shop:"woolingsworth", price: 4},
-          {shop: "kwakspar", price: 4},
-          {shop: "shopwrong", price: 2},
-        ]
-      );
-
-  });
-
+    var result = Fruit.descendingApples({
+   "woolingsworth": {
+     "apples": 4,
+     "bananas": 3,
+     "oranges": 12
+   },
+   "chockers": {
+     "bananas": 2,
+     "apples": 5,
+     "oranges": 4
+   },
+   "picklepay": {
+     "bananas": 4,
+     "oranges": 7
+   },
+   "shopwrong": {
+     "apples": 2,
+     "bananas": 3
+   },
+   "kwakspar": {
+     "oranges": 9,
+     "apples": 4
+}
+   });
+   assert.deepEqual(result,
+[ { shop: 'chockers', price: 5 },
+{ shop: 'woolingsworth', price: 4 },
+{ shop: 'kwakspar', price: 4 },
+{ shop: 'shopwrong', price: 2 } ]
+);
+});
 });
 
 
@@ -152,7 +187,7 @@ describe("cheapestFruit", function(){
        }
 
       );
-      assert.equal = (result,  1);
+      assert.equal(result,  1);
 
   });
 
